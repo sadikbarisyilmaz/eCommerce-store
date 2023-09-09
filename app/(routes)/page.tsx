b4d771a9235cfad8ded1b5712568acf871507e85
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-09-09 19:47:50 +0300

    Feat: categories and billboards fetched

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 import React from "react";
+export const revalidate = 0;
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+const HomePage = async () => {
+  const billboard = await getBillboard("97ac548a-433d-44e6-a4a3-e24d28d6a934");
+  const products = await getProducts({ isFeatured: true });
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import Container from "@/components/ui/contaier";
+import Container from "@/components/ui/container";
 import Link from "next/link";
+import MainNav from "@/components/main-nav";
+import getCategories from "@/actions/get-categories";
 
-const Navbar = () => {
+export const revalidate = 0;
+
+const Navbar = async () => {
+  const categories = await getCategories();
+
   return (
     <div className="border-b">
       <Container>
@@ -10,6 +16,7 @@ const Navbar = () => {
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl ">STORE</p>
           </Link>
+          <MainNav data={categories} />
         </div>
       </Container>
     </div>
